f7fb4f2a266047612b14056ae481635e20be902d
Daniel Fernando <117900695+Danielfukdw@users.noreply.github.com>
2022-11-17 16:24:08 +0700

    Add files via upload

---
+++ Soal1_D_71220894.py
@@ -0,0 +1,4 @@
+phi=3.14
+y = float(input("Masukan jari-jari:"))
+LuasLingkaran= phi*y*y
+print (str(LuasLingkaran))
---
+++ Soal2_D_71220894.py
@@ -0,0 +1,5 @@
+x = float(input("Masukan Alas Atap:"))
+y = float(input("Masukan tinggi Atap"))
+z = float (input("Masukan tinggi tembok:"))
+z=
+Print ("Rumah tersebut membutuhkan"+)
